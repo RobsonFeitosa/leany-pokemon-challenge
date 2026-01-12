5805456fcd8cf17a9de71011cf2d5c1d0d4926f3
Robson <robson.gw@hotmail.com>
2026-01-11 21:45:20 -0300

    feat: implementa configuracao do typeorm e mapper global

---
+++ src/shared/infra/database/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+export const dataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'fcac41078f2abd6c272b8601a8511f0c',
+    database: 'bp-nexus',
+    entities: [
+        __dirname + '/../**/*.entity{.ts,.js}',
+    ],
+    migrations: [
+        __dirname + '/migrations/*.{ts,js}'
+    ],
+    migrationsTableName: 'migrations',
+    synchronize: true,
+});
